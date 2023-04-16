97a9aed4a1ed7d6ffd0ee28e3b7abe91eb289efd
Hung Pham <tienhung278@gmail.com>
2023-04-16 09:46:35 +0800

    Change application name. End commit

--- src/main/java/com/aquariux/trading/TradingApplication.java
+++ src/main/java/com/aquariux/trading/TradingApplication.java
@@ -10,10 +10,10 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-public class SchedulerApplication {
+public class TradingApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SchedulerApplication.class, args);
+		SpringApplication.run(TradingApplication.class, args);
 	}
 
 	@Bean
